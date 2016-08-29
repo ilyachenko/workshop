e0ca059fcd81895bb0a1294ae8fa06a2bb86201a
Ilya Dyachenko <ilya.dyachenko@gmail.com>
2016-08-29 12:35:57 +0300

    done

--- 06-content-negotiation/index.js
+++ 06-content-negotiation/index.js
@@ -15,5 +15,11 @@ var gzip = require('mz/zlib').gzip;
 var app = module.exports = koa();
 
 app.use(function* () {
-
+	if(this.acceptsEncodings('gzip')){
+		this.response.set('Content-Encoding', 'gzip');
+		this.response.body = yield gzip('hello world');
+	} 
+	else if(this.acceptsEncodings('identity')) {
+		this.response.body = 'hello world';
+	}
 })
